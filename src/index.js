a1d6c5d8b7c5ee0e396b569979f72296ac8bf1f9
veronika447 <starenkaya.veronika@gmail.com>
2023-02-15 17:40:26 +0300

    feat: add function towel-soft

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix == undefined)
+    return [];
+  let result = [];
+  matrix = matrix.map((item, index) => {
+    if (index % 2 === 1)
+      item.reverse();
+    item.map(el => result.push(el));
+  })
+  return result;
 }
